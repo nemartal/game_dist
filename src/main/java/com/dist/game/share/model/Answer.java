a02c88117210f351430908b4f43e2a81b5c8210f
Fermin Jimenez <fejimea@unirioja.es>
2021-01-04 19:49:36 +0100

    Question & Answer

---
+++ src/main/java/com/dist/game/share/model/Answer.java
@@ -0,0 +1,10 @@
+package com.dist.game.share.model;
+
+public class Answer {
+
+    private String id;
+
+    private String text;
+
+    private boolean correct;
+}
--- src/main/java/com/dist/game/share/model/Question.java
+++ src/main/java/com/dist/game/share/model/Question.java
@@ -1,6 +1,11 @@
 package com.dist.game.share.model;
 
+import java.util.ArrayList;
+
 public class Question {
 
     private String id;
+
+    private ArrayList<Answer> answers;
+
 }
