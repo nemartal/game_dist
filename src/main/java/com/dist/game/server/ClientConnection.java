12226d9795a05c69e74a6f049676bfc1214a3d5c
Fermin Jimenez <fejimea@unirioja.es>
2021-01-04 20:36:05 +0100

    Client Connection - Read data

--- src/main/java/com/dist/game/server/ClientConnection.java
+++ src/main/java/com/dist/game/server/ClientConnection.java
@@ -1,7 +1,12 @@
 package com.dist.game.server;
 
 import com.dist.game.share.model.User;
+import com.dist.game.share.model.dto.UserDTO;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class ClientConnection extends Thread {
@@ -16,9 +21,28 @@ public class ClientConnection extends Thread {
     @Override
     public void run() {
 
-        // GET nickname
-        // Create or join
-        // Play
+        InputStream is = null;
+        OutputStream os = null;
 
+        ObjectInputStream ois = null;
+        try {
+            is = socket.getInputStream();
+            os = socket.getOutputStream();
+
+            ois = new ObjectInputStream(is);
+
+            // Get Nickname
+            UserDTO userDto = (UserDTO) ois.readObject();
+
+            // Create or join
+            // Play
+
+        } catch (IOException e) {
+            System.out.println("Error: IOException");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: ClassNotFoundException");
+            e.printStackTrace();
+        }
     }
 }
