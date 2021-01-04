104c77a5cda1e776d3f03da08f80fe50b128f3e5
Fermin <fjimenez@ginzo.tech>
2021-01-04 19:40:38 +0100

    Base

---
+++ src/main/java/com/dist/game/share/model/Question.java
@@ -0,0 +1,4 @@
+package com.dist.game.share.model;
+
+public class Question {
+}
---
+++ src/main/java/com/dist/game/share/model/User.java
@@ -0,0 +1,42 @@
+package com.dist.game.share.model;
+
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+public class User {
+
+    private final String id;
+    private final String nickname;
+
+    public User() {
+        this.id = UUID.randomUUID().toString();
+        this.nickname = "user_" + (new Date().getTime());
+    }
+
+    public User(String nickname) {
+        this.id = UUID.randomUUID().toString();
+        this.nickname = nickname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
