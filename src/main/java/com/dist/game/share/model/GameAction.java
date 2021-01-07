e53c78f27c1994ac6ace7c916a9adbd544e74650
Fermin Jimenez <fejimea@unirioja.es>
2021-01-07 18:19:30 +0100

    Client Connection server

---
+++ src/main/java/com/dist/game/share/model/GameAction.java
@@ -0,0 +1,8 @@
+package com.dist.game.share.model;
+
+import java.io.Serializable;
+
+public class GameAction implements Serializable {
+    public static final String CREATE = "create";
+    public static final String JOIN = "join";
+}
