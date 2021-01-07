9e4afaa21b4785192792f954a8a66e24163e524b
nemartal <43754337+nemartal@users.noreply.github.com>
2021-01-07 18:19:47 +0100

    Client

--- src/main/java/com/dist/game/client/gui/NickIntro.java
+++ src/main/java/com/dist/game/client/gui/NickIntro.java
@@ -4,38 +4,34 @@ import javafx.scene.control.Labeled;
 
 import javax.swing.*;
 import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class NickIntro {
     private JPanel rootPanel;
     private JTextArea textArea;
     private JButton accessButton;
     private JTextField textField1;
+    private JPanel panel1;
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-
-        // Get the size of the screen
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-
-        // Determine the new location of the window
-        int w = rootPanel.getSize().width;
-        int h = rootPanel.getSize().height;
-        int x = (dim.width-w)/2;
-        int y = (dim.height-h)/2;
-
-        // Move the window
-        rootPanel.setLocation(x, y);
+    /*public NickIntro() {
+        $$$setupUI$$$();
     }
 
+    private void createUIComponents() {
+        textArea = new JTextArea();
+        textArea.setText("Introduce Nick");
+    }
+
+    private void $$$setupUI$$$() {
+        createUIComponents();
+    }*/
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("NickIntro");
+        JFrame frame = new JFrame("Preguntados");
         frame.setContentPane(new NickIntro().rootPanel);
-        new NickIntro().createUIComponents();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
-
-
 }
