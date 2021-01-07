83d2b49fd1711da8ae65b23dbdad2ea8186849d7
Fermin Jimenez <fejimea@unirioja.es>
2021-01-07 20:02:35 +0100

    Game type

--- src/main/java/com/dist/game/server/controller/GameControllerParty.java
+++ src/main/java/com/dist/game/server/controller/GameControllerParty.java
@@ -1,9 +1,11 @@
 package com.dist.game.server.controller;
 
+import com.dist.game.share.model.GameType;
+
 public class GameControllerParty extends GameController {
 
-    public GameControllerParty() {
-        super();
+    public GameControllerParty(GameType type) {
+        super(type);
     }
 
 }
--- src/main/java/com/dist/game/server/controller/GameControllerSpeed.java
+++ src/main/java/com/dist/game/server/controller/GameControllerSpeed.java
@@ -1,9 +1,11 @@
 package com.dist.game.server.controller;
 
+import com.dist.game.share.model.GameType;
+
 public class GameControllerSpeed extends GameController {
 
-    public GameControllerSpeed() {
-        super();
+    public GameControllerSpeed(GameType type) {
+        super(type);
     }
 
 }
